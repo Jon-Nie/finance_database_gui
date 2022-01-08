3e3fcb034207bdaadb41e441fc771a1d986295c0
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-01-08 21:12:43 +0100

    adding different section classes

--- stock_page.py
+++ stock_page.py
@@ -5,6 +5,7 @@ from base_classes import Page
 import numpy as np
 import queries
 
+
 class StockPage(Page):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -67,10 +68,12 @@ class StockPage(Page):
         self.header_section.update_data(data)
         self.overview_section.update_data(data)
 
+
 class OrientationBar(QFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setFixedWidth(200)
+
 
 class BoxTemplate(QFrame):
     def __init__(self, header_label, icon_path, *args, **kwargs):
@@ -100,6 +103,7 @@ class BoxTemplate(QFrame):
         self.header = BoxHeader(header_label, icon_path)
         self.layout.addWidget(self.header)
 
+
 class BoxHeader(QLabel):
     def __init__(self, text, icon_path, *args, **kwargs):
         super().__init__(text, *args, **kwargs)
@@ -114,3 +118,144 @@ class BoxHeader(QLabel):
         )
 
         self.icon_path = icon_path
+
+
+class HeaderSection(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setObjectName("HeaderSection")
+
+        self.layout = QGridLayout(self)
+        self.layout.setHorizontalSpacing(20)
+        self.layout.setAlignment(Qt.AlignLeft)
+
+        self.logo = QLabel()
+        self.layout.addWidget(self.logo, 0, 0, 2, 2)
+
+        self.name = QLabel()
+        self.name.setStyleSheet(
+            """
+            QLabel {
+                font-family: Lato;
+                font-weight: Bold;
+                font-size: 28px;
+            }
+            """
+        )
+        self.layout.addWidget(self.name, 0, 2, 1, 3)
+
+        self.ticker = QLabel()
+        self.ticker.setStyleSheet(
+            """
+            QLabel {
+                font-family: Lato;
+                font-weight: Bold;
+                font-size: 20px;
+            }
+            """
+        )
+        self.layout.addWidget(self.ticker, 1, 2)
+
+        self.isin = QLabel()
+        self.isin.setStyleSheet(
+            """
+            QLabel {
+                font-family: Lato;
+                font-weight: Bold;
+                font-size: 20px;
+            }
+            """
+        )
+        self.layout.addWidget(self.isin, 1, 3)
+
+        self.price = QLabel()
+        self.price.setStyleSheet(
+            """
+            QLabel {
+                font-family: Lato;
+                font-weight: Bold;
+                font-size: 20px;
+            }
+            """
+        )
+        self.layout.addWidget(self.price, 1, 4)
+
+        self.layout.addWidget(QFrame(), 1, 5)
+    
+    def update_data(self, data):
+        profile = data["profile"]
+        pixmap = QPixmap()
+        pixmap.loadFromData(profile["logo"])
+        pixmap = pixmap.scaledToWidth(100)
+        self.logo.setPixmap(pixmap)
+
+        self.name.setText(profile["name"])
+        self.ticker.setText(profile["ticker"])
+        self.isin.setText(profile["isin"])
+        self.price.setText(f"{data['time_series']['close'].iloc[-1]:.2f} {profile['currency']}")
+
+
+class OverviewSection(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setObjectName("OverviewSection")
+
+        self.layout = QGridLayout(self)
+        self.layout.setContentsMargins(10, 10, 10, 10)
+        self.layout.setHorizontalSpacing(15)
+        self.layout.setVerticalSpacing(25)
+
+        self.profile_box = ProfileBox("Profile", None)
+        self.layout.addWidget(self.profile_box, 0, 0)
+
+        self.key_data_box = KeyDataBox("Key Data", None)
+        self.layout.addWidget(self.key_data_box, 0, 1)
+
+        self.description_box = DescriptionBox("Business Description", None)
+        self.layout.addWidget(self.description_box, 1, 0, 1, 2)
+
+        self.chart_box = PriceChartBox("Price Chart", None)
+        self.layout.addWidget(self.chart_box, 2, 0)
+
+        self.snapshot_box = FundamentalSnapshotBox("Fundamental Snapshot", None)
+        self.layout.addWidget(self.snapshot_box, 3, 0)
+
+        self.news_box = NewsBox("News", None)
+        self.layout.addWidget(self.news_box, 2, 1, 2, 1)
+
+    def update_data(self, data):
+        self.profile_box.update_data(data["profile"])
+        self.key_data_box.update_data(data["time_series"], data["fundamentals"], data["profile"]["currency"])
+        self.description_box.description.setText(data["profile"]["description"])
+        self.chart_box.update_data(data["time_series"])
+        self.snapshot_box.update_data(data["time_series"])
+        self.news_box.update_data(data["news"])
+
+
+class PriceSection(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.layout = QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+
+class FundamentalsSection(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.layout = QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+
+class HoldersSection(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.layout = QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+
+class FilingsSection(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.layout = QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
