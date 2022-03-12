697ae9e409c7eadff7825ab2a3bb452898479477
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-03-12 19:38:40 +0100

    reset layout

--- base_classes.py
+++
@@ -1,7 +0,0 @@
-from PySide2.QtCore import *
-from PySide2.QtWidgets import *
-from PySide2.QtGui import *
-
-class Page(QFrame):
-    def __init__(self):
-        super().__init__()
--- commodities_page.py
+++
@@ -1,8 +0,0 @@
-from PySide2.QtCore import *
-from PySide2.QtWidgets import *
-from PySide2.QtGui import *
-from base_classes import Page
-
-class CommoditiesPage(Page):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
