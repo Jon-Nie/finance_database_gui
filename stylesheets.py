bcc2acbbd3cf6b9b14111c8b7ebdd787a76b13c9
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-03-15 23:02:33 +0100

    adding stylesheets

---
+++ stylesheets.py
@@ -0,0 +1,39 @@
+app_css = """
+    QFrame {
+        background-color: #FFFFFF
+    }
+"""
+
+sidebar_css = """
+    QFrame {
+        background-color: #161A28
+    }
+"""
+
+sidebar_button_css = """
+    QPushButton {{
+        border: None;
+        color: #A1A1A1;
+        font-family: Lato;
+        font-size: 14px;
+        text-align: left;
+        font-weight: bold;
+        padding: 15px 0px 15px 15px;
+        icon: url(icons/{0}_grey.svg);
+        qproperty-iconSize: 20px
+    }}
+
+    QPushButton:hover {{
+        color: #FFFFFF;
+        background-color: #20263A;
+        icon: url(icons/{0}_white.svg)
+    }}
+
+    QPushButton#active {{
+        border-left: 3px solid #FFFFFF;
+        color: #FFFFFF;
+        background-color: #20263A;
+        padding: 10px 0px 10px 12px;
+        icon: url(icons/{0}_white.svg)
+    }}
+"""
