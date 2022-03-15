1f8faf0d7a3352b66e895907193ff06aa6fed1da
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-03-15 21:43:08 +0100

    bundling pages

--- pages.py
+++ pages.py
@@ -4,6 +4,9 @@ from PySide2.QtGui import *
 from home_page import HomePage
 from equities_page import EquitiesPage
 from stock_page import StockPage
+from institutionals_page import InstitutionalsPage
+from industries_page import IndustriesPage
+from analysts_page import AnalystsPage
 from commodities_page import CommoditiesPage
 from economics_page import EconomicDataPage
 from tracker_page import PortfolioTrackerPage
@@ -16,6 +19,9 @@ class Pages(QStackedWidget):
         self.home_page = HomePage()
         self.equities_page = EquitiesPage()
         self.stock_page = StockPage()
+        self.institutionals_page = InstitutionalsPage()
+        self.industries_page = IndustriesPage()
+        self.analysts_page = AnalystsPage()
         self.commodities_page = CommoditiesPage()
         self.economics_page = EconomicDataPage()
         self.tracker_page = PortfolioTrackerPage()
@@ -25,6 +31,9 @@ class Pages(QStackedWidget):
             self.home_page,
             self.equities_page,
             self.stock_page,
+            self.institutionals_page,
+            self.industries_page,
+            self.analysts_page,
             self.commodities_page,
             self.economics_page,
             self.tracker_page,
