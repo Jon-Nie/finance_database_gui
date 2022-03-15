c12e5941ea5f22b765fbafe95f7adaace2247da9
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-03-15 21:36:59 +0100

    adding basic files

---
+++ app.py
@@ -0,0 +1,15 @@
+import sys
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+if __name__ == "__main__":
+    app = QApplication()
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
