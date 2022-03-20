c8a5dc905ec815a12aa614e9f22727893ca92ba0
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-03-20 13:52:22 +0100

    adding sidebar button style

--- stylesheets.py
+++ stylesheets.py
@@ -10,15 +10,17 @@ sidebar_css = """
     }
 """
 
+
+
 sidebar_button_css = """
     QPushButton {{
-        border: None;
-        color: #A1A1A1;
         font-family: Lato;
         font-size: 14px;
         text-align: left;
         font-weight: regular;
-        padding: 15px 0px 15px 15px;
+        color: #A1A1A1;
+        border: None;
+        padding: 10px 0px 10px 15px;
         icon: url(icons/{0}_grey.svg);
         qproperty-iconSize: 20px
     }}
@@ -36,10 +38,75 @@ sidebar_button_css = """
     }}
 
     QPushButton#active {{
-        border-left: 3px solid #FFFFFF;
+        font-family: Lato;
+        font-size: 14px;
+        text-align: left;
+        font-weight: regular;
         color: #FFFFFF;
         background-color: #20263A;
+        border: None;
+        border-left: 3px solid #FFFFFF;
         padding: 10px 0px 10px 12px;
         icon: url(icons/{0}_white.svg)
     }}
+"""
+
+sidebar_button_super_css = """
+    QPushButton {{
+        font-family: Lato;
+        font-size: 14px;
+        text-align: left;
+        font-weight: regular;
+        color: #A1A1A1;
+        border: None;
+        padding: 10px 0px 10px 15px;
+        icon: url(icons/{0}_grey.svg);
+        qproperty-iconSize: 20px
+    }}
+
+    QPushButton:hover {{
+        color: #FFFFFF;
+        background-color: #20263A;
+        icon: url(icons/{0}_white.svg)
+    }}
+
+    QPushButton:pressed {{
+        color: #FFFFFF;
+        background-color: #161A28;
+        icon: url(icons/{0}_white.svg)
+    }}
+
+    QPushButton#active {{
+        color: #FFFFFF;
+        icon: url(icons/{0}_white.svg)
+    }}
+"""
+
+sidebar_button_sub_css = """
+    QPushButton {
+        border: None;
+        color: #A1A1A1;
+        font-family: Lato;
+        font-size: 13px;
+        text-align: left;
+        font-weight: regular;
+        padding: 0px 0px 0px 40px;
+    }
+
+    QPushButton:hover {
+        color: #FFFFFF;
+        background-color: #20263A;
+    }
+
+    QPushButton:pressed {
+        color: #FFFFFF;
+        background-color: #161A28;
+    }
+
+    QPushButton#active {
+        border-left: 3px solid #FFFFFF;
+        color: #FFFFFF;
+        background-color: #20263A;
+        padding: 0px 0px 0px 37px;
+    }
 """
