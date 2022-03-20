6f8c3d8376c72a9daf99c354ffccbe683c99461b
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-03-20 13:54:19 +0100

    updating pages

--- pages.py
+++ pages.py
@@ -3,10 +3,12 @@ from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from home_page import HomePage
 from equities_page import EquitiesPage
+from stocks_page import StocksPage
 from stock_page import StockPage
 from institutionals_page import InstitutionalsPage
 from industries_page import IndustriesPage
 from analysts_page import AnalystsPage
+from factors_page import FactorsPage
 from commodities_page import CommoditiesPage
 from economics_page import EconomicDataPage
 from tracker_page import PortfolioTrackerPage
@@ -17,11 +19,12 @@ class Pages(QStackedWidget):
         super().__init__()
 
         self.home_page = HomePage()
-        self.equities_page = EquitiesPage()
+        self.stocks_page = StocksPage()
         self.stock_page = StockPage()
-        self.institutionals_page = InstitutionalsPage()
         self.industries_page = IndustriesPage()
         self.analysts_page = AnalystsPage()
+        self.institutionals_page = InstitutionalsPage()
+        self.factors_page = FactorsPage()
         self.commodities_page = CommoditiesPage()
         self.economics_page = EconomicDataPage()
         self.tracker_page = PortfolioTrackerPage()
@@ -29,11 +32,12 @@ class Pages(QStackedWidget):
 
         self.pages = (
             self.home_page,
-            self.equities_page,
+            self.stocks_page,
             self.stock_page,
-            self.institutionals_page,
             self.industries_page,
             self.analysts_page,
+            self.institutionals_page,
+            self.factors_page,
             self.commodities_page,
             self.economics_page,
             self.tracker_page,
