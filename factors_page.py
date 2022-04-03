1ba240793f877b7f07ce2e32f83ef457fe33166b
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-04-03 17:07:15 +0200

    once again design reset

--- factors_page.py
+++
@@ -1,7 +0,0 @@
-from PySide2.QtCore import *
-from PySide2.QtWidgets import *
-from PySide2.QtGui import *
-
-class FactorsPage(QFrame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
