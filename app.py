ecb98879ce5c44e97c898b4e2132ed68035c8a51
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-04-05 20:31:27 +0200

    adding resizing grips for all edges

--- app.py
+++ app.py
@@ -34,21 +34,68 @@ class MainWindow(QMainWindow):
         self.content.topbar.close_button.clicked.connect(self.close)
 
         self.content.topbar.mouseMoveEvent = self.moveWindow
+
+
+        self.size_grip_left = QFrame(self)
+        self.size_grip_left.setStyleSheet(grip_css)
+        self.size_grip_left.setCursor(Qt.SizeHorCursor)
+        self.size_grip_left.installEventFilter(self)
+
+        self.size_grip_top = QFrame(self)
+        self.size_grip_top.setStyleSheet(grip_css)
+        self.size_grip_top.setCursor(Qt.SizeVerCursor)
+        self.size_grip_top.installEventFilter(self)
+
+        self.size_grip_right = QFrame(self)
+        self.size_grip_right.setStyleSheet(grip_css)
+        self.size_grip_right.setCursor(Qt.SizeHorCursor)
+        self.size_grip_right.installEventFilter(self)
+
+        self.size_grip_bottom = QFrame(self)
+        self.size_grip_bottom.setStyleSheet(grip_css)
+        self.size_grip_bottom.setCursor(Qt.SizeVerCursor)
+        self.size_grip_bottom.installEventFilter(self)
+
         self.size_grip_top_left = QSizeGrip(self)
         self.size_grip_top_left.setStyleSheet(grip_css)
+
         self.size_grip_top_right = QSizeGrip(self)
         self.size_grip_top_right.setStyleSheet(grip_css)
+
         self.size_grip_bottom_right = QSizeGrip(self)
         self.size_grip_bottom_right.setStyleSheet(grip_css)
+
         self.size_grip_bottom_left = QSizeGrip(self)
         self.size_grip_bottom_left.setStyleSheet(grip_css)
     
     def paintEvent(self, event):
-        width, height = 10, 10
+        width, height = 5, 5
+
+        self.size_grip_left.setGeometry(0, 0, width, self.height())
+        self.size_grip_top.setGeometry(0, 0, self.width(), height)
+        self.size_grip_right.setGeometry(self.width() - width, 0, width, self.height())
+        self.size_grip_bottom.setGeometry(0, self.height() - height, self.width(), height)
+        
         self.size_grip_top_left.setGeometry(0, 0, width, height)
-        self.size_grip_top_right.setGeometry(self.width()-width, 0, width, height)
-        self.size_grip_bottom_right.setGeometry(self.width()-width, self.height()-height, width, height)
-        self.size_grip_bottom_left.setGeometry(0, self.height()-height, width, height)
+        self.size_grip_top_right.setGeometry(self.width() - width, 0, width, height)
+        self.size_grip_bottom_right.setGeometry(self.width() - width, self.height() - height, width, height)
+        self.size_grip_bottom_left.setGeometry(0, self.height() - height, width, height)
+    
+    def eventFilter(self, object, event):
+        if event.type() == QEvent.MouseMove:
+            mouse_position = event.globalPos()
+            width = self.geometry().width()
+            height = self.geometry().height()
+            if object == self.size_grip_left:
+                self.setGeometry(mouse_position.x(), self.geometry().y(), self.geometry().x() + width - mouse_position.x(), height)
+            if object == self.size_grip_top:
+                self.setGeometry(self.geometry().x(), mouse_position.y(), width, self.geometry().y() + height - mouse_position.y())
+            if object == self.size_grip_right:
+                self.setGeometry(self.geometry().x(), self.geometry().y(), mouse_position.x() - self.geometry().x(), height)
+            if object == self.size_grip_bottom:
+                self.setGeometry(self.geometry().x(), self.geometry().y(), width, mouse_position.y() - self.geometry().y())
+            return True
+        return super().eventFilter(object, event)
 
     def moveWindow(self, event):
         if event.buttons() == Qt.LeftButton:
