7b5e36898757517485be6cee341ba79ae7c9777e
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-04-05 23:27:54 +0200

    inheriting from Page

--- pages/analysts_page.py
+++ pages/analysts_page.py
@@ -1,7 +1,8 @@
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
+from .page import Page
 
-class AnalystsPage(QFrame):
+class AnalystsPage(Page):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- pages/home_page.py
+++ pages/home_page.py
@@ -1,7 +1,15 @@
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
+from .page import Page
 
-class HomePage(QFrame):
+class HomePage(Page):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+    
+    def background(self):
+        return """
+            QFrame {
+                background-color: #3E75C8
+            }
+        """
