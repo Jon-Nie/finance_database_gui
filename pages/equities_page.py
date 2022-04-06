600d980d04ab2d79f3529078d420f45937fe49b8
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-04-06 20:55:32 +0200

    implementing most of the home/equities layout and functionalities

--- pages/equities_page.py
+++ pages/equities_page.py
@@ -2,7 +2,64 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from .page import Page
+from finance_database_gui.shared_widgets import *
+from finance_database_gui.utils import equity_page_description
 
 class EquitiesPage(Page):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.layout = QGridLayout(self)
+        self.layout.setContentsMargins(50, 50, 50, 50)
+        self.layout.setSpacing(20)
+
+        self.header = SectionHeader(color="white", text="Equities")
+        self.layout.addWidget(self.header, 0, 0)
+
+        self.description = SectionDescription(
+            color="white",
+            text=equity_page_description
+        )
+        self.layout.addWidget(self.description, 1, 0)
+
+        self.stocks_box = SectionCard(
+            color="white",
+            text="Stocks",
+            description="Browse for stocks with the best fundamental conditions. See, how analysts and institutional money managers view the stock or find uncovered value stocks.",
+            icon="stocks"
+        )
+        self.layout.addWidget(self.stocks_box, 1, 1)
+
+        self.industries_box = SectionCard(
+            color="white",
+            text="Industries",
+            description="Examine, how a company can be compared to its competitors and whether there are superior companies of the same industry.",
+            icon="stocks"
+        )
+        self.layout.addWidget(self.industries_box, 1, 2)
+
+        self.analysts_box = SectionCard(
+            color="white",
+            text="Analysts",
+            description="See, how big money managers performed in the past and what their portfolios look like.",
+            icon="stocks"
+        )
+        self.layout.addWidget(self.analysts_box, 2, 0)
+
+        self.institutionals_box = SectionCard(
+            color="white",
+            text="Institutionals",
+            description="Distinguish good and bad analysts and explore, what stocks they recommend and how accurate their recommendations were.",
+            icon="stocks"
+        )
+        self.layout.addWidget(self.institutionals_box, 2, 1)
+
+        self.factors_box = SectionCard(
+            color="white",
+            text="Factors",
+            description="Explore academic variable-sorted portfolios, compute their factor exposure and find out, what works particularly well in the stock market.",
+            icon="stocks"
+        )
+        self.layout.addWidget(self.factors_box, 2, 2)
+
+        self.layout.addWidget(QFrame(), 3, 0)
--- pages/home_page.py
+++ pages/home_page.py
@@ -2,11 +2,68 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from .page import Page
+from finance_database_gui.shared_widgets import *
+from finance_database_gui.utils import home_page_description
 
 class HomePage(Page):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    
+
+        self.layout = QGridLayout(self)
+        self.layout.setContentsMargins(50, 50, 50, 50)
+        self.layout.setSpacing(20)
+
+        self.header = SectionHeader(color="blue", text="Finance Database GUI")
+        self.layout.addWidget(self.header, 0, 0)
+
+        self.description = SectionDescription(
+            color="blue",
+            text=home_page_description
+        )
+        self.layout.addWidget(self.description, 1, 0)
+
+        self.equities_box = SectionCard(
+            color="blue",
+            text="Equities",
+            description="Compare different ETFs with each other, develop a good grasp of a stocks' outlook and build efficient portfolios.",
+            icon="stocks"
+        )
+        self.layout.addWidget(self.equities_box, 1, 1)
+
+        self.commodities_box = SectionCard(
+            color="blue",
+            text="Commodities",
+            description="Examine, how different commodities performed in the past, how their returns could be explained and pick the commodities with the highest premia.",
+            icon="stocks"
+        )
+        self.layout.addWidget(self.commodities_box, 1, 2)
+
+        self.economics_box = SectionCard(
+            color="blue",
+            text="Economics",
+            description="Explore a rich dataset of macroeconomic variables and build your investment thesis upon it.",
+            icon="stocks"
+        )
+        self.layout.addWidget(self.economics_box, 2, 0)
+
+        self.tracker_box = SectionCard(
+            color="blue",
+            text="Portfolio Tracker",
+            description="See, how your portfolio performed compared to a benchmark and examine its factor exposure.",
+            icon="stocks"
+        )
+        self.layout.addWidget(self.tracker_box, 2, 1)
+
+        self.backtests_box = SectionCard(
+            color="blue",
+            text="Backtests",
+            description="Backtest your strategies with an easy-to-use frontend that lets you pick variables, timeframes etc. and returns a comprehensive overview of investment strategies.",
+            icon="stocks"
+        )
+        self.layout.addWidget(self.backtests_box, 2, 2)
+
+        self.layout.addWidget(QFrame(), 3, 0)
+
     def background(self):
         return """
             QFrame {
