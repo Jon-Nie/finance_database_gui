32b2139803c12ad014aa68986d35719a9e457285
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-04-06 23:38:59 +0200

    switching layout

--- pages/stocks_page.py
+++ pages/stocks_page.py
@@ -7,31 +7,3 @@ from finance_database_gui.shared_widgets import ContentBox
 class StocksPage(Page):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self.layout = QGridLayout(self)
-
-        self.name = ContentBox(self)
-        self.layout.addWidget(self.name, 0, 0, 1, 3)
-        self.name.setMaximumHeight(50)
-
-        self.news = ContentBox(self)
-        self.layout.addWidget(self.news, 0, 3, 1, 3)
-
-        self.description = ContentBox(self)
-        self.layout.addWidget(self.description, 1, 0, 1, 4)
-
-        self.prices = ContentBox(self)
-        self.layout.addWidget(self.prices, 1, 4, 2, 2)
-
-        self.analysts = ContentBox(self)
-        self.layout.addWidget(self.analysts, 2, 0, 1, 1)
-
-        self.value = ContentBox(self)
-        self.layout.addWidget(self.value, 2, 1, 1, 1)
-
-        self.profitability = ContentBox(self)
-        self.layout.addWidget(self.profitability, 2, 2, 1, 1)
-        self.profitability.setMaximumWidth(50)
-
-        self.growth = ContentBox(self)
-        self.layout.addWidget(self.growth, 2, 3, 1, 1)
