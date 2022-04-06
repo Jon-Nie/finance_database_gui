d136b1b8f5e171c26a1493f3d8ed1beda2a7965b
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-04-06 21:22:19 +0200

    outsourcing variables to utils file

---
+++ utils.py
@@ -0,0 +1,13 @@
+home_page_description = """
+<p>This app covers every aspect an investor is interested in. Browse for stocks, industries, analysts or big money managers and find new ideas.</p>
+Develop your investment case with macroeconomic and stock-related data, backtest it and assess the performance with standard academic techniques.
+<p>Build a real-time factor portfolio, either with stocks or commodities, which have proven to outperform the market.</p>
+<p>The app is split in 5 major parts. Alternatively, search directly for a specific entity (e.g. AAPL).</p>
+"""
+
+equity_page_description = """
+<p>Equities should be the main component of a portfolio as they have high risk premia.</p>
+<p>Browse for stocks and get a quick grasp of their fundamental situation. Compare companies within an industry or pick 
+stocks with high exposure to return boosting factors. See, how analysts view your stocks and whether big money managers are also invested.</p>
+<p>No matter what is important to look at for a stock, this section has you covered.</p>
+"""
