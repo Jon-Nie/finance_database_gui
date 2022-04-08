05a39c19aa007754dad44742aef4942ddb5c2375
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-04-08 14:28:53 +0200

    adding Custom Label parent class, removing letter spacing

--- shared_widgets.py
+++ shared_widgets.py
@@ -2,6 +2,13 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 
+class Label(QLabel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setTextInteractionFlags(Qt.TextSelectableByMouse)
+        self.setCursor(Qt.IBeamCursor)
+
+
 class ContentBox(QFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -36,20 +43,12 @@ class SectionCard(QFrame):
 
         self.header = CardHeader(text)
         self.layout.addWidget(self.header)
-        font = QFont()
-        font.setLetterSpacing(QFont.AbsoluteSpacing, 1.05)
-        self.header.setFont(font)
 
-        self.description = QLabel(self)
+        self.description = Label(self)
         self.description.setAlignment(Qt.AlignAbsolute)
         self.description.setText(description)
         self.description.setObjectName("description")
         self.description.setWordWrap(True)
-        self.description.setTextInteractionFlags(Qt.TextSelectableByMouse)
-        self.description.setCursor(Qt.IBeamCursor)
-        font = QFont()
-        font.setLetterSpacing(QFont.AbsoluteSpacing, 1.02)
-        self.description.setFont(font)
         self.layout.addWidget(self.description)
 
         self.button = QPushButton(self)
@@ -118,24 +117,24 @@ class SectionCard(QFrame):
             """
         )
 
-class CardHeader(QLabel):
+class CardHeader(Label):
     def __init__(self, text, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setObjectName("header")
         self.setMaximumHeight(30)
         self.setText(text)
-        self.setTextInteractionFlags(Qt.TextSelectableByMouse)
-        self.setCursor(Qt.IBeamCursor)
+        font = QFont()
+        font.setWeight(QFont.ExtraBold)
+        self.setFont(font)
 
-class CardButton(QLabel):
+class CardButton(Label):
     def __init__(self, text, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setText(text)
         self.setStyleSheet("border: 1px solid black")
-        self.setTextInteractionFlags(Qt.TextSelectableByMouse)
-        self.setCursor(Qt.IBeamCursor)
 
-class SectionDescription(QLabel):
+
+class SectionDescription(Label):
     def __init__(self, color, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setMaximumHeight(230)
@@ -148,8 +147,6 @@ class SectionDescription(QLabel):
             border = "#3971C6"
 
         self.setWordWrap(True)
-        self.setTextInteractionFlags(Qt.TextSelectableByMouse)
-        self.setCursor(Qt.IBeamCursor)
 
         self.setStyleSheet(
             f"""
@@ -164,11 +161,8 @@ class SectionDescription(QLabel):
             }}
             """
         )
-        font = QFont()
-        font.setLetterSpacing(QFont.AbsoluteSpacing, 1.02)
-        self.setFont(font)
 
-class SectionHeader(QLabel):
+class SectionHeader(Label):
     def __init__(self, color, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
@@ -176,9 +170,6 @@ class SectionHeader(QLabel):
             font = "#FFFFFF"
         elif color == "white":
             font = "#333333"
-        
-        self.setTextInteractionFlags(Qt.TextSelectableByMouse)
-        self.setCursor(Qt.IBeamCursor)
 
         self.setStyleSheet(
             f"""
@@ -190,7 +181,3 @@ class SectionHeader(QLabel):
             }}
             """
         )
-
-        font = QFont()
-        font.setLetterSpacing(QFont.AbsoluteSpacing, 1.05)
-        self.setFont(font)
